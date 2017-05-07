0b62a22c2a3b0607f2d55732b091a82e6a55caef
mwojtow <marcin713@wp.pl>
2017-05-07 17:59:25 +0200

    Adding files

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+new WOW().init();
+
+//Smooth scroll
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+      // Store hash
+      var hash = this.hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+           // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+//Validating email address
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function validate() {
+  $("#result").text("");
+  var email = $("#email").val();
+  if (validateEmail(email)) {
+    $("#result").text("Thank you. Addres " + email + " has been added to newsletter.");
+    $("#result").css("color", "green");
+  } else {
+    $("#result").text(email + " is not correct.");
+    $("#result").css("color", "red");
+  }
+  return false;
+}
+
+$("#signup").bind("click", validate);
